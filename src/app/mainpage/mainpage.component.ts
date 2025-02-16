07408f09ee8534d5b396201d1e1b77cc6ea99f0e
YOUR NAME <YOUR EMAIL@SANPLECOM>
2025-02-16 15:22:58 +0300

    task 6,7

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -1,9 +1,12 @@
+import { NgIf } from "@angular/common";
 import { Component } from "@angular/core";
 
 const newPages = [5, 4, 3, 2, 1];
 
 @Component({
     selector: 'app-mainpage',
+    standalone: true,
+    imports: [NgIf],
     templateUrl: './mainpage.component.html',
     styleUrl: './mainpage.component.scss'
 })
