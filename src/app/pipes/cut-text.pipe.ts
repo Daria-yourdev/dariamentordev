8292c9983a0d9fbb2c96fd96126d4ea458be2194
daria-yourdev <daspirogova@gmail.com>
2025-04-13 16:25:03 +0300

    formatting

--- src/app/pipes/cut-text.pipe.ts
+++ src/app/pipes/cut-text.pipe.ts
@@ -1,16 +1,11 @@
 import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-    name: 'twenty_symbolmax',
+    name: 'cutText',
     standalone: true
 })
 export class TwentySymbolMaxPipe implements PipeTransform {
     transform(text: string): string {
-        if (text.length > 20) {
-            return text.substring(0, 20) + '...'
-        } else {
-            return text;
-        }
+        return text.length > 17 ? text.substring(0, 17) + '...' : text;
     }
-
 }
